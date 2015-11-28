8618ee26b2dd55c2aead7c8c6282440a39915fb7
exercise <orcawhale@yeah.net>
2015-11-28 11:28:20 +0800

    Update search_range.py

--- search_range.py
+++ search_range.py
@@ -39,3 +39,60 @@ class Solution(object):
             return [-1, -1]
 
         return [nums.index(target), len(nums) - nums[::-1].index(target) -1 ]
+
+    def searchRange(self, nums, target):
+       # Runtime: 44 ms
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        start , end = 0, len(nums) -1
+        while start < end:
+            mid = (start+ end) / 2
+            if target <=  nums[mid]:
+                end = mid
+            else:
+                start = mid +1
+        if nums[start] != target:
+            return [-1, -1]
+        left = start
+        
+        start , end = 0, len(nums[left:])
+        while start < end:
+            mid = (start+ end) / 2
+            if target < nums[left+mid]:
+                end = mid
+            else:
+                start = mid +1 
+        
+        return [left, left+start-1]
+
+    def searchRange(self, nums, target):
+      # Runtime: 48 ms
+      """
+      :type nums: List[int]
+      :type target: int
+      :rtype: List[int]
+      """
+      start , end = 0, len(nums) -1
+        while start < end:
+            mid = (start+ end) / 2
+            if target <=  nums[mid]:
+                end = mid
+            else:
+                start = mid +1
+        if nums[start] != target:
+            return [-1, -1]
+        range = [start]
+
+        start , end = 0, len(nums)
+        while start < end:
+            mid = (start+ end) / 2
+            if target < nums[mid]:
+                end = mid
+            else:
+                start = mid +1 
+        
+        return range + [start -1]
+        
