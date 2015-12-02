a79ee885b1ea22c0cd536c7d66e36cb3d4b8ac89
exercise <orcawhale@yeah.net>
2015-12-03 02:09:51 +0800

    Create find-minimum-in-rotated-sorted-arra.py

---
+++ find-minimum-in-rotated-sorted-arra.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def findMin(self, nums):
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) / 2
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+        return nums[left]
